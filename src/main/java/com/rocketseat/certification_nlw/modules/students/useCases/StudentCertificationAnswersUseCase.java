bd356472e274a17b16643e640b4cdbda52700fdb
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-02-08 12:08:39 -0300

    feat: criando serviço de geração de certificado.

--- src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/StudentCertificationAnswersUseCase.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/useCases/StudentCertificationAnswersUseCase.java
@@ -1,23 +1,103 @@
 package com.rocketseat.certification_nlw.modules.students.useCases;
 
+import com.rocketseat.certification_nlw.modules.questions.entities.AlternativesEntity;
+import com.rocketseat.certification_nlw.modules.questions.entities.QuestionEntity;
+import com.rocketseat.certification_nlw.modules.questions.repositories.QuestionRepository;
+import com.rocketseat.certification_nlw.modules.students.dto.StudentCertificationAnswerDTO;
+import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.entities.AnswersCertificationsEntity;
+import com.rocketseat.certification_nlw.modules.students.entities.CertificationStudentEntity;
+import com.rocketseat.certification_nlw.modules.students.entities.StudentEntity;
+import com.rocketseat.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 import com.rocketseat.certification_nlw.modules.students.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class StudentCertificationAnswersUseCase {
     @Autowired
     private StudentRepository studentRepository;
 
-//    public void execute(StudentCertificationAnswerDTO dto) {
-//        // Verificar se usuário ele existe
-//        var student = studentRepository.findByEmail(dto.getEmail());
-//
-//        if(student)
-//
-//        // Buscar as alternativas das perguntas
-//        // - Correct ou Incorreta
-//
-//        // Salvar as informações da certificação
-//    }
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
+    @Autowired
+    private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
+    public CertificationStudentEntity execute(StudentCertificationAnswerDTO dto) throws Exception {
+        var hasCertification = this.verifyIfHasCertificationUseCase
+                .execute(new VerifyHasCertificationDTO(dto.getEmail(), dto.getTechnology()));
+
+        if (hasCertification) {
+            throw new Exception("Você já tirou sua certificação!");
+        }
+
+        // Buscar as alternativas das perguntas
+        // - Correct ou Incorreta
+        List<QuestionEntity> questionsEntity = questionRepository.findByTechnology(dto.getTechnology());
+        List<AnswersCertificationsEntity> answersCertifications = new ArrayList<>();
+
+        AtomicInteger correctAnswers = new AtomicInteger(0);
+
+        dto.getQuestionsAnswers().forEach(questionAnswer -> {
+            var question = questionsEntity.stream()
+                    .filter(q -> q.getId().equals(questionAnswer.getQuestionID())).findFirst().get();
+
+            var findCorrectAlternative = question.getAlternatives().stream()
+                    .filter(AlternativesEntity::isCorrect).findFirst().get();
+
+            if (findCorrectAlternative.getId().equals(questionAnswer.getAlternativeID())) {
+                questionAnswer.setCorrect(true);
+                correctAnswers.incrementAndGet();
+            } else {
+                questionAnswer.setCorrect(false);
+            }
+
+            var answersCertificationsEntity = AnswersCertificationsEntity.builder()
+                    .answerId(questionAnswer.getAlternativeID())
+                    .questionId(questionAnswer.getQuestionID())
+                    .isCorrect(questionAnswer.isCorrect()).build();
+
+            answersCertifications.add(answersCertificationsEntity);
+        });
+
+        // Verificar se existe student pelo email
+        var student = studentRepository.findByEmail(dto.getEmail());
+        UUID studentID;
+        if (student.isEmpty()) {
+            var studentCreated = StudentEntity.builder().email(dto.getEmail()).build();
+            studentCreated = studentRepository.save(studentCreated);
+            studentID = studentCreated.getId();
+        } else {
+            studentID = student.get().getId();
+        }
+
+        CertificationStudentEntity certificationStudentEntity = CertificationStudentEntity.builder()
+                .technology(dto.getTechnology())
+                .studentID(studentID)
+                .grade(correctAnswers.get())
+                .build();
+
+        var certificationStudentCreated = certificationStudentRepository.save(certificationStudentEntity);
+
+        answersCertifications.forEach(answerCertification -> {
+            answerCertification.setCertificationId(certificationStudentEntity.getId());
+            answerCertification.setCertificationStudentEntity(certificationStudentEntity);
+        });
+
+        certificationStudentEntity.setAnswersCertificationsEntities(answersCertifications);
+
+        // Salvar as informações da certificação
+        certificationStudentRepository.save(certificationStudentEntity);
+
+        return certificationStudentCreated;
+    }
 }
