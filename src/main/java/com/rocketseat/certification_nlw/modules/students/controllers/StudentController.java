944308e450fec69355bdd3cac54b6aa8b0cb7ab5
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2024-02-08 12:23:08 -0300

    feat: rota para buscar respostas do certificado.

--- src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -1,8 +1,11 @@
 package com.rocketseat.certification_nlw.modules.students.controllers;
 
+import com.rocketseat.certification_nlw.modules.students.dto.StudentCertificationAnswerDTO;
 import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.useCases.StudentCertificationAnswersUseCase;
 import com.rocketseat.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +17,25 @@ public class StudentController {
     @Autowired
     private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
 
+    @Autowired
+    private StudentCertificationAnswersUseCase studentCertificationAnswersUseCase;
+
     @PostMapping("/verifyIfHasCertification")
     public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO) {
         var result = this.verifyIfHasCertificationUseCase.execute(verifyHasCertificationDTO);
 
         return result ? "Usuário já fez a prova" : "Usuário pode fazer a prova";
+    }
+
+    @PostMapping("/certification/answer")
+    public ResponseEntity<Object> certificationAnswer(
+            @RequestBody StudentCertificationAnswerDTO studentCertificationAnswerDTO) {
+        try {
+            var result = studentCertificationAnswersUseCase.execute(studentCertificationAnswerDTO);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+
     }
 }
